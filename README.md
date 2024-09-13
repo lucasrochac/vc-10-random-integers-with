90c6b23f6034fc3acb45f0a33437d61c61167acd
lucasrochasc <lucas_rochadasilvacruz@hotmail.com>
2024-09-13 16:39:11 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Random Number Generator with Repetitions
+
+This project is a simple C# application that generates a list of random integers with specific repetition requirements. The list contains 10 numbers where at least one number repeats twice and another number repeats three times.
+
+## Functionality
+
+The application performs the following:
+- Generates a list of 10 random integers between 1 and 50.
+- Ensures that at least one number repeats exactly twice.
+- Ensures that at least one other number repeats exactly three times.
+- Displays the final list of numbers.
---
+++ vc-10-random-integers-with-repetitions/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class MainClass
+{
+    public static void Main(string[] args)
+    {
+        List<int> original = new List<int>();
+        Random random = new Random();
+
+        FillListWithNumbers(original, random, 5);
+
+        EnsureNumberRepeats(original, random, 2);
+        EnsureNumberRepeats(original, random, 3);
+
+        Console.WriteLine(string.Format("Original: {0}", string.Join(", ", original)));
+    }
+
+    private static void FillListWithNumbers(List<int> list, Random random, int count)
+    {
+        if (list.Count >= count)
+        {
+            return;
+        }
+
+        int newNumber = random.Next(1, 51);
+        list.Add(newNumber);
+
+        FillListWithNumbers(list, random, count);
+    }
+
+    private static void EnsureNumberRepeats(List<int> list, Random random, int repeatCount)
+    {
+        if (repeatCount == 2)
+        {
+            int repeatTwice = random.Next(1, 51);
+            
+            while (list.Contains(repeatTwice))
+            {
+                repeatTwice = random.Next(1, 51);
+            }
+
+            list.Add(repeatTwice);
+            list.Add(repeatTwice);
+        }
+        else if (repeatCount == 3)
+        {
+            int repeatThrice = random.Next(1, 51);
+            
+            while (list.Contains(repeatThrice))
+            {
+                repeatThrice = random.Next(1, 51);
+            }
+
+            list.Add(repeatThrice);
+            list.Add(repeatThrice);
+            list.Add(repeatThrice);
+        }
+    }
+}
